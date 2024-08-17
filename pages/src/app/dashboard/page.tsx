4a8b2413a265fb5ec95133f63318311ae1d8a270
khanzadigithubid <memonbisma49@gmail.com>
2024-08-18 01:38:22 +0500

    This is Next.js Pages

---
+++ pages/src/app/dashboard/page.tsx
@@ -0,0 +1,25 @@
+const Dashboard = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            textAlign: 'center'
+        }}>
+        <h2>
+        Welcome to Dashbord!
+        </h2>
+        </div>
+    )
+}
+
+export default Dashboard;
+
+
+/* Pages
+
+In Next.js, pages are created by adding files to the pages directory. Each file in this directory corresponds to a route in your application.
+
+This will be accessible at / in your application */
+
---
+++ pages/src/app/home/page.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            textAlign: 'center'
+        }}>
+        <h2>
+        Welcome to Home!
+        </h2>
+        </div>
+    )
+}
+
+export default Home;
